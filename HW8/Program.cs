5a0dfe509ceb1c75d502b31d442946e3465a6398
Assel_Lindt <papina_fotokartochka@mail.ru>
2023-02-19 22:03:16 +0600

    HomeWork #10

--- HW8/Program.cs
+++ HW8/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.Write("Enter a number: ");
 int n = int.Parse(Console.ReadLine());
 for (int i = 2; i <= n; i++)
-    if(i % 2 == 0)
     {
         Console.WriteLine(i);
     }
---
+++ HomeWork_sem2_#10/Program.cs
@@ -0,0 +1,5 @@
+﻿int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+
+int result = a / 10 % 10;
+Console.WriteLine(result);
