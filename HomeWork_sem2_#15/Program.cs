4c171282027c58adfef0237c4dc3893816be1956
Assel_Lindt <papina_fotokartochka@mail.ru>
2023-02-19 22:16:17 +0600

    HomeWork_sem2_#15

---
+++ HomeWork_sem2_#15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a <= 7)
+{
+    if (a == 6 || a == 7)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+else
+{
+    Console.WriteLine("Число не входит в заданный диапазон");
+}
