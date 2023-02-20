955374d567f7c613a8cce7da5a6175b028a9266f
Assel_Lindt <papina_fotokartochka@mail.ru>
2023-02-20 11:52:04 +0600

    HomeWork_sem2_#13

---
+++ HomeWork_sem2_#13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите любое число :");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    while (n / 1000 != 0)
+    {
+        n = n / 10;
+    }
+int result = n % 10;
+Console.WriteLine(result);
+}
