ecb0ba23d1b6c871c12eeb4dd4957fe625ea486a
Assel_Lindt <papina_fotokartochka@mail.ru>
2023-02-23 00:16:44 +0600

    HomeWork_Sem3_#23

---
+++ HomeWork_Sem3_#23/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int m = N;
+
+if (N < 0)
+{
+    i = N;//-10
+    m = -N;//10
+    N = -1;//-1
+}
+int[] result = new int[m];
+
+int j = 0;
+
+
+for (j = 0; i <= N; i++)
+{
+    result[j] = i * i * i;
+    j++;
+}
+for (j = 0; j < m; j++)
+{
+    Console.WriteLine($"{result[j]} ");
+}
