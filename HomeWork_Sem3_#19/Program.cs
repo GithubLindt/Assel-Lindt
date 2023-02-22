8219a454c6405f9cabde122a9c7e7abadd4306d0
Assel_Lindt <papina_fotokartochka@mail.ru>
2023-02-23 00:53:22 +0600

    HomeWork_Sem3_#19

---
+++ HomeWork_Sem3_#19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int N = int.Parse(Console.ReadLine());
+
+int a1 = N / 10000;
+int a2 = N / 1000 % 10;
+int a4 = N / 10 % 10;
+int a5 = N % 10;
+
+if (a1 == a5 && a2 == a4)
+{
+    Console.WriteLine("-> yes");
+}
+else
+{
+    Console.WriteLine("-> no");
+}
