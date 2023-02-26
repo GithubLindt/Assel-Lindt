63951da636c74b7f84cdbd97f9434bfe32129519
Assel_Lindt <papina_fotokartochka@mail.ru>
2023-02-26 22:27:58 +0600

    HomeWork_sem4_#27

---
+++ HomeWork_sem4_#27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int number){
+    
+    int count = Convert.ToString(number).Length;
+    int s = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++){
+      s = number - number % 10;
+      result = result + (number - s);
+      number = number / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр в числе = " + sumNumber);
