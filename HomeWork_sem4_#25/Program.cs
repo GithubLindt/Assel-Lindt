3f73fe69aae34c5e3a66a56d379df4ae2ab19002
Assel_Lindt <papina_fotokartochka@mail.ru>
2023-02-26 21:20:07 +0600

    HomeWork_sem4_#25

---
+++ HomeWork_sem4_#25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+
+int number = (int)Math.Pow(a,b);
+Console.WriteLine($"Возведение числа 1 в степень числа 2 = {number}");
