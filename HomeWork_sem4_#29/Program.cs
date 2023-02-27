18367d728252cfc5266000a019a997d44f8da7eb
Assel_Lindt <papina_fotokartochka@mail.ru>
2023-02-27 14:53:41 +0600

    t

---
+++ HomeWork_sem4_#29/Program.cs
@@ -0,0 +1,13 @@
+﻿var array = new int[8];
+
+for(int i = 0; i <= array.Length - 1; i++) {
+    array[i] = GenerateDigit();    
+}
+
+int GenerateDigit() 
+{
+    Random randomNumber = new Random();
+    return randomNumber.Next(100);
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", array));
